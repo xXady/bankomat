f7c0f5c2534bc4d57706e8e8bb377c17f1962c1d
xXady <102038515+xXady@users.noreply.github.com>
2023-10-16 14:21:56 +0500

    numbers_1.py

--- numbers_1.py
+++ numbers_1.py
@@ -2,6 +2,10 @@ from num2words import num2words
 
 n = int(input("Введите любое число:"))
 
+if n < 1 or n > 100000:
+    print("Вы ввели неправильное число!")
+    n = int(input("Введите число от 1 до 100000:"))
+
 p = num2words(n, lang='ru')
 
 if n % 10 == 1 and n != 11:
